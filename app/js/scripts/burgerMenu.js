3cf0e3a3c48d5ce08f6e873669f8048f6daa3540
ekaterina-s <catshavrina@gmail.com>
2021-06-25 23:17:39 +0200

    changes

--- app/js/scripts/burgerMenu.js
+++ app/js/scripts/burgerMenu.js
@@ -3,6 +3,25 @@ const navigationPanel = document.querySelector('.nav');
 const blackout = document.querySelector('._blackout');
 
 burgerMenu.addEventListener('click', () => {
-    navigationPanel.style.display ="block";
-    blackout.style.display ="block";
+    navigationPanel.style.display = "block";
+    blackout.style.display = "block";
 });
+
+const navLink = document.querySelectorAll('.nav__link');
+const dropdownMenu = document.querySelectorAll('.dropdown');
+const dropdownLink = document.querySelectorAll('.dropdown__link');
+console.log(dropdownLink);
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', () => {
+        navLink[i].classList.add("_active");
+        dropdownMenu[0].classList.add("_active");
+    });
+};
+
+for (let j = 0; j < dropdownLink.length; j++) {
+    dropdownLink[j].addEventListener('click', () => {
+        dropdownLink[j].classList.add("_active");
+        dropdownMenu[1].classList.add("_active");
+    });
+};
